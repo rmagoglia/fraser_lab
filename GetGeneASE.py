0f15cba37001c6e152696da61e284d20d5d8970f
Rachel Agoglia <rmagoglia@gmail.com>
2016-05-04 21:13:48 -0700

    Cast read count variables into ints prior to summing for non-stranded data

--- GetGeneASE.py
+++ GetGeneASE.py
@@ -358,8 +358,8 @@ for line in gff_file:
 				if name not in alt_biased:
 					alt_biased[name] = 0
 
-				tot_ref = ref_pos_counts + ref_neg_counts
-				tot_alt = alt_pos_counts + alt_neg_counts
+				tot_ref = int(ref_pos_counts) + int(ref_neg_counts)
+				tot_alt = int(alt_pos_counts) + int(alt_neg_counts)
 				if tot_ref + tot_alt >= args.min:
 					if tot_ref > tot_alt:
 						if name in ref_biased:
